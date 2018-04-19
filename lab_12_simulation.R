0825065cf3ccd50a7f8cfd44cb3403eafad3e75c
sunnianyi <dnsun@andrew.cmu.edu>
2018-04-19 17:05:01 -0400

    Added model_select, which keeps only those covariates with p-value <= cutoff.

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -1,3 +1,10 @@
 generate_data = function(n, p) {
   list(covariates = matrix(rnorm(n*p), n, p), responses = rnorm(n))
 }
+
+model_select = function(covariates, responses, cutoff) {
+  p.values = summary(lm(responses ~ covariates))$coefficients[,4][-1] # remove intercept
+  if (all(p.values > cutoff)) return(c()) # none of the p-values meet the cutoff
+  summary(lm(responses ~ covariates[,p.values <= cutoff]))$coefficients[,4] # alt regression
+}
+
