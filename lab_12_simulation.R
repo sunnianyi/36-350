42fc9bd248470485f62555291dd122d8383c8948
sunnianyi <dnsun@andrew.cmu.edu>
2018-04-19 19:08:32 -0400

    Fixed a p.values bug in model_select, added run_simulation and histogram plots for 9 combinations of n and p.

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -3,8 +3,23 @@ generate_data = function(n, p) {
 }
 
 model_select = function(covariates, responses, cutoff) {
-  p.values = summary(lm(responses ~ covariates))$coefficients[,4][-1] # remove intercept
+  p.values = head(summary(lm(responses ~ covariates))$coefficients[,4], -1)
   if (all(p.values > cutoff)) return(c()) # none of the p-values meet the cutoff
   summary(lm(responses ~ covariates[,p.values <= cutoff]))$coefficients[,4] # alt regression
 }
 
+run_simulation = function(n_trials, n, p, cutoff) {
+  p.values = c()
+  for (i in 1:n_trials) {
+    data = generate_data(n, p)
+    p.values = c(p.values, as.vector(model_select(data$covariates, data$responses, cutoff)))
+  }
+  hist(p.values, xlab="p-values", main=paste("n = ", n, ", p = ", p, sep=''))
+}
+
+par(mfrow=c(3,3), mar=c(4,4,4,2))
+for (n in c(100, 1000, 10000)) {
+  for (p in c(10, 20, 50)) {
+    run_simulation(100, n, p, 0.05)
+  }
+}
